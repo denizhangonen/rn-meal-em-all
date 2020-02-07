3bb29680a89ec72604f4177799aef925ac41156d
Denizhan Gönen <denizhangonen@Denizhans-MacBook-Pro.local>
2020-02-07 19:31:11 +0300

    navigation added

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,13 +1,24 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, FlatList } from "react-native";
+
+import { CATEGORIES } from "../data/dummy-data";
+
+const renderGridItem = itemData => {
+  return (
+    <View style={styles.gridItem}>
+      <Text>{itemData.item.title}</Text>
+    </View>
+  );
+};
 
 const CategoriesScreen = props => {
-  console.log(props)
   return (
-    <View style={styles.screen}>
-      <Text>CategoriesScreen</Text>
-      <Button title="Click Me" onPress={() => {}} />
-    </View>
+    <FlatList
+      keyExtractor={(item, index) => item.id}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+    />
   );
 };
 
@@ -16,6 +27,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height:150
   }
 });
 
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 const CategoryMealsScreen = props => {
   return (
     <View style={styles.screen}>
       <Text>CategoryMealsScreen</Text>
+      <Button
+        title="Meal Detail"
+        onPress={() => {
+          props.navigation.navigate({
+            routeName: "MealDetail"
+          });
+        }}
+      />
     </View>
   );
 };
